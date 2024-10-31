d8ec77cb88ea04ac97c6d7a619ece5368af26389
Jonas Lima de Amorim <dev.amorim@outlook.com>
2024-10-30 22:26:19 -0300

    refactor: changing height for aspect

--- components/ActivityChart.tsx
+++ components/ActivityChart.tsx
@@ -39,7 +39,7 @@ export const ActivityChart: FunctionComponent<IActivityChart> = ({
   }
 
   return (
-    <ResponsiveContainer width="100%" height="100%">
+    <ResponsiveContainer width="100%" aspect={1}>
       <AreaChart data={data} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
         <defs>
           <linearGradient id="gradient" x1="0" y1="0" x2="0" y2="1">
