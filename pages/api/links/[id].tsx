658438d8e2079408a827a0e7f3c58edb8fa75956
Jonas Lima de Amorim <dev.amorim@outlook.com>
2024-11-15 03:26:23 -0300

    feat: implementing delete method on local API

--- pages/api/links/[id].tsx
+++ pages/api/links/[id].tsx
@@ -36,8 +36,19 @@ export default withApiAuthRequired(async (req, res) => {
 
       return res.status(response.status).json(await response.json());
 
+    case "DELETE":
+      url = new URL(`${process.env.NEXT_PUBLIC_API_BASE_URL}/Links/${id}`);
+      response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      return res.status(response.status).json(await response.json());
+
     default:
-      res.setHeader("Allow", ["GET", "PUT"]);
+      res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
       res.status(405).end(`Method ${req.method} Not Allowed`);
       return res;
   }
