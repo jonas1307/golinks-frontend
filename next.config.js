a60b66b5156d053da9fc4b6a3566495e45b20d81
Jonas Lima de Amorim <dev.amorim@outlook.com>
2026-06-23 19:44:49 -0300

    fix: restore nodeMiddleware experimental flag required for Auth0 v4

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ if (process.env.NODE_ENV === 'development') {
 
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    nodeMiddleware: true,
+  },
   images: {
     remotePatterns: [
       {
